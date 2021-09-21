f4c3617800eb18368678dd72938c75182688a7f5
RecordBreakerOG <djbarrett@gmail.com>
2021-09-21 13:51:46 +1000

    Published shit

---
+++ assets/preload/data/eggnog/modchart.lua
@@ -0,0 +1,29 @@
+-- this gets called starts when the level loads.
+function start(song) -- arguments, the song name
+	local defaultPos = 0
+	note.getPosition(defaultPos)
+end
+
+-- this gets called every frame
+function update(elapsed) -- arguments, how long it took to complete a frame
+	if curBeat > 71 and curBeat < 120 then
+		local currentBeat = (songPos / 1000)*(bpm/60)
+		for i=0,7 do
+			setActorX(_G['defaultStrum'..i..'X'] + 32 * math.sin((currentBeat + i*0.25) * math.pi), i)
+		end
+	end
+	if curBeat == 120 then
+		tweenPosX(i, _G['defaultStrum'..i..'X'], 0, 0.6, 'setDefault')
+		tweenPosY(i, _G['defaultStrum'..i..'Y'], 0, 0.6, 'setDefault')
+	end
+end
+
+-- this gets called every beat
+function beatHit(beat) -- arguments, the current beat of the song
+
+end
+
+-- this gets called every step
+function stepHit(step) -- arguments, the current step of the song (4 steps are in a beat)
+
+end
